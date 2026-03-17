bb8ffba7ae045bcf001f0a35d8383a1221aba2cc
Olim Yuldoshev <oyuldoshev39@gmail.com>
2026-03-17 15:59:25 +0500

    Added

--- src/pages/borrowedBooks/BorrowedBooks.tsx
+++ src/pages/borrowedBooks/BorrowedBooks.tsx
@@ -8,34 +8,37 @@ import { LuPlus } from "react-icons/lu";
 // import InputLabel from "@mui/material/InputLabel";
 // import Select from "@mui/material/Select";
 // import MenuItem from "@mui/material/MenuItem";
-import { useMemo, useState } from "react";
+import {
+  // useMemo,
+  useState
+} from "react";
 
 //Material UI
 import {
-  alpha,
+  // alpha,
   styled,
   // useTheme
 } from "@mui/material/styles";
-import Box from "@mui/material/Box";
+// import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
+// import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
-import TableSortLabel from "@mui/material/TableSortLabel";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
-import { visuallyHidden } from "@mui/utils";
+// import TableSortLabel from "@mui/material/TableSortLabel";
+// import Toolbar from "@mui/material/Toolbar";
+// import Typography from "@mui/material/Typography";
+// import Paper from "@mui/material/Paper";
+// import { visuallyHidden } from "@mui/utils";
 import { IoClose } from "react-icons/io5";
 import { AiFillEdit } from "react-icons/ai";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogActions from "@mui/material/DialogActions";
 import { MdDelete } from "react-icons/md";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const BorrowedBooks = () => {
   const allFiltersByCategory: any = [
@@ -105,12 +108,12 @@ const BorrowedBooks = () => {
   ];
 
 
-  const [order, setOrder] = useState<Order>("asc");
-  const [orderBy, setOrderBy] = useState<any>("bookTitle");
-  const [selected, setSelected] = useState<readonly number[]>([]);
-  const [page, setPage] = useState(0);
+  // const [order, setOrder] = useState<Order>("asc");
+  // const [orderBy, setOrderBy] = useState<any>("bookTitle");
+  // const [selected, setSelected] = useState<readonly number[]>([]);
+  // const [page, setPage] = useState(0);
   // const [dense, setDense] = useState(false);
-  const [rowsPerPage, setRowsPerPage] = useState<number>(17);
+  // const [rowsPerPage, setRowsPerPage] = useState<number>(17);
   const [modalFilter, setModalFilter] = useState<boolean>(false);
   const [modalShowAllFilters, setModalShowAllFilters] =
     useState<boolean>(false);
@@ -157,229 +160,229 @@ const BorrowedBooks = () => {
     },
   }));
 
-  // Table Section
-  const rows: any = [
-    {
-      id: 1,
-      img: "/src/assets/signIn/logo-pehraft-sign-in.svg",
-      bookTitle: "Cashflow Quadrant",
-      author: "Robert Kiyosaki",
-      category: "Finance",
-      bookPage: 256,
-      status: "Available",
-    },
-  ];
+  // // Table Section
+  // const rows: any = [
+  //   {
+  //     id: 1,
+  //     img: "/src/assets/signIn/logo-pehraft-sign-in.svg",
+  //     bookTitle: "Cashflow Quadrant",
+  //     author: "Robert Kiyosaki",
+  //     category: "Finance",
+  //     bookPage: 256,
+  //     status: "Available",
+  //   },
+  // ];
 
-  function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-    if (b[orderBy] < a[orderBy]) {
-      return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-      return 1;
-    }
-    return 0;
-  }
-
-  type Order = "asc" | "desc";
-
-  function getComparator<Key extends keyof any>(
-    order: Order,
-    orderBy: Key,
-  ): (
-    a: { [key in Key]: number | string },
-    b: { [key in Key]: number | string },
-  ) => number {
-    return order === "desc"
-      ? (a, b) => descendingComparator(a, b, orderBy)
-      : (a, b) => -descendingComparator(a, b, orderBy);
-  }
-
-  const headCells: any = [
-    {
-      id: "img",
-      numeric: false,
-      disablePadding: true,
-      label: "Image",
-    },
-    {
-      id: "bookTitle",
-      numeric: false,
-      disablePadding: true,
-      label: "Book Title",
-    },
-    {
-      id: "borrower-name",
-      numeric: false,
-      disablePadding: false,
-      label: "Borrower Name",
-    },
-    {
-      id: "date-borrowed",
-      numeric: false,
-      disablePadding: false,
-      label: "Date Borrowed",
-    },
-    {
-      id: "due-date",
-      numeric: false,
-      disablePadding: false,
-      label: "Due",
-    },
-    {
-      id: "phone-number",
-      numeric: false,
-      disablePadding: false,
-      label: "Phone number",
-    },
-  ];
-
-  interface EnhancedTableProps {
-    numSelected: number;
-    onRequestSort: (event: React.MouseEvent<unknown>, property: any) => void;
-    onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    order: Order;
-    orderBy: string;
-    rowCount: number;
-  }
-
-  function EnhancedTableHead(props: EnhancedTableProps) {
-    const {
-      // onSelectAllClick,
-      order,
-      orderBy,
-      // numSelected,
-      // rowCount,
-      onRequestSort,
-    } = props;
-    const createSortHandler =
-      (property: any) => (event: React.MouseEvent<unknown>) => {
-        onRequestSort(event, property);
-      };
-
-    return (
-      <TableHead>
-        <TableRow>
-          {headCells.map((headCell: any) => (
-            <TableCell
-              key={headCell.id}
-              // align={headCell.numeric ? "right" : "left"}
-              padding={headCell.disablePadding ? "none" : "normal"}
-              sortDirection={orderBy === headCell.id ? order : false}
-            >
-              <TableSortLabel
-                active={orderBy === headCell.id}
-                direction={orderBy === headCell.id ? order : "asc"}
-                onClick={createSortHandler(headCell.id)}
-              >
-                {headCell.label}
-                {orderBy === headCell.id ? (
-                  <Box component="span" sx={visuallyHidden}>
-                    {order === "desc"
-                      ? "sorted descending"
-                      : "sorted ascending"}
-                  </Box>
-                ) : null}
-              </TableSortLabel>
-            </TableCell>
-          ))}
-        </TableRow>
-      </TableHead>
-    );
-  }
-
-  interface EnhancedTableToolbarProps {
-    numSelected: number;
-  }
-
-  function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
-    const { numSelected } = props;
-
-    return (
-      <Toolbar
-        sx={{
-          pl: { sm: 2 },
-          pr: { xs: 1, sm: 1 },
-          ...(numSelected > 0 && {
-            bgcolor: (theme) =>
-              alpha(
-                theme.palette.primary.main,
-                theme.palette.action.activatedOpacity,
-              ),
-          }),
-        }}
-      >
-        <Typography
-          sx={{ flex: "1 1 100%" }}
-          variant="h6"
-          id="tableTitle"
-          component="div"
-        >
-          Books
-        </Typography>
-      </Toolbar>
-    );
-  }
-
-  const handleRequestSort = (_: React.MouseEvent<unknown>, property: any) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    setOrderBy(property);
-  };
-
-  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      const newSelected = rows.map((n: any) => n.id);
-      setSelected(newSelected);
-      return;
-    }
-    setSelected([]);
-  };
-
-  // const handleClick = (_: React.MouseEvent<unknown>, id: number) => {
-  //   const selectedIndex = selected.indexOf(id);
-  //   let newSelected: readonly number[] = [];
-
-  //   if (selectedIndex === -1) {
-  //     newSelected = newSelected.concat(selected, id);
-  //   } else if (selectedIndex === 0) {
-  //     newSelected = newSelected.concat(selected.slice(1));
-  //   } else if (selectedIndex === selected.length - 1) {
-  //     newSelected = newSelected.concat(selected.slice(0, -1));
-  //   } else if (selectedIndex > 0) {
-  //     newSelected = newSelected.concat(
-  //       selected.slice(0, selectedIndex),
-  //       selected.slice(selectedIndex + 1),
-  //     );
+  // function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  //   if (b[orderBy] < a[orderBy]) {
+  //     return -1;
   //   }
-  //   setSelected(newSelected);
+  //   if (b[orderBy] > a[orderBy]) {
+  //     return 1;
+  //   }
+  //   return 0;
+  // }
+
+  // type Order = "asc" | "desc";
+
+  // function getComparator<Key extends keyof any>(
+  //   order: Order,
+  //   orderBy: Key,
+  // ): (
+  //   a: { [key in Key]: number | string },
+  //   b: { [key in Key]: number | string },
+  // ) => number {
+  //   return order === "desc"
+  //     ? (a, b) => descendingComparator(a, b, orderBy)
+  //     : (a, b) => -descendingComparator(a, b, orderBy);
+  // }
+
+  // const headCells: any = [
+  //   {
+  //     id: "img",
+  //     numeric: false,
+  //     disablePadding: true,
+  //     label: "Image",
+  //   },
+  //   {
+  //     id: "bookTitle",
+  //     numeric: false,
+  //     disablePadding: true,
+  //     label: "Book Title",
+  //   },
+  //   {
+  //     id: "borrower-name",
+  //     numeric: false,
+  //     disablePadding: false,
+  //     label: "Borrower Name",
+  //   },
+  //   {
+  //     id: "date-borrowed",
+  //     numeric: false,
+  //     disablePadding: false,
+  //     label: "Date Borrowed",
+  //   },
+  //   {
+  //     id: "due-date",
+  //     numeric: false,
+  //     disablePadding: false,
+  //     label: "Due",
+  //   },
+  //   {
+  //     id: "phone-number",
+  //     numeric: false,
+  //     disablePadding: false,
+  //     label: "Phone number",
+  //   },
+  // ];
+
+  // interface EnhancedTableProps {
+  //   numSelected: number;
+  //   onRequestSort: (event: React.MouseEvent<unknown>, property: any) => void;
+  //   onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  //   order: Order;
+  //   orderBy: string;
+  //   rowCount: number;
+  // }
+
+  // function EnhancedTableHead(props: EnhancedTableProps) {
+  //   const {
+  //     // onSelectAllClick,
+  //     order,
+  //     orderBy,
+  //     // numSelected,
+  //     // rowCount,
+  //     onRequestSort,
+  //   } = props;
+  //   const createSortHandler =
+  //     (property: any) => (event: React.MouseEvent<unknown>) => {
+  //       onRequestSort(event, property);
+  //     };
+
+  //   return (
+  //     <TableHead>
+  //       <TableRow>
+  //         {headCells.map((headCell: any) => (
+  //           <TableCell
+  //             key={headCell.id}
+  //             // align={headCell.numeric ? "right" : "left"}
+  //             padding={headCell.disablePadding ? "none" : "normal"}
+  //             sortDirection={orderBy === headCell.id ? order : false}
+  //           >
+  //             <TableSortLabel
+  //               active={orderBy === headCell.id}
+  //               direction={orderBy === headCell.id ? order : "asc"}
+  //               onClick={createSortHandler(headCell.id)}
+  //             >
+  //               {headCell.label}
+  //               {orderBy === headCell.id ? (
+  //                 <Box component="span" sx={visuallyHidden}>
+  //                   {order === "desc"
+  //                     ? "sorted descending"
+  //                     : "sorted ascending"}
+  //                 </Box>
+  //               ) : null}
+  //             </TableSortLabel>
+  //           </TableCell>
+  //         ))}
+  //       </TableRow>
+  //     </TableHead>
+  //   );
+  // }
+
+  // interface EnhancedTableToolbarProps {
+  //   numSelected: number;
+  // }
+
+  // function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
+  //   const { numSelected } = props;
+
+  //   return (
+  //     <Toolbar
+  //       sx={{
+  //         pl: { sm: 2 },
+  //         pr: { xs: 1, sm: 1 },
+  //         ...(numSelected > 0 && {
+  //           bgcolor: (theme) =>
+  //             alpha(
+  //               theme.palette.primary.main,
+  //               theme.palette.action.activatedOpacity,
+  //             ),
+  //         }),
+  //       }}
+  //     >
+  //       <Typography
+  //         sx={{ flex: "1 1 100%" }}
+  //         variant="h6"
+  //         id="tableTitle"
+  //         component="div"
+  //       >
+  //         Books
+  //       </Typography>
+  //     </Toolbar>
+  //   );
+  // }
+
+  // const handleRequestSort = (_: React.MouseEvent<unknown>, property: any) => {
+  //   const isAsc = orderBy === property && order === "asc";
+  //   setOrder(isAsc ? "desc" : "asc");
+  //   setOrderBy(property);
   // };
 
-  const handleChangePage = (_: unknown, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  // const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setDense(event.target.checked);
+  // const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   if (event.target.checked) {
+  //     const newSelected = rows.map((n: any) => n.id);
+  //     setSelected(newSelected);
+  //     return;
+  //   }
+  //   setSelected([]);
   // };
 
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+  // // const handleClick = (_: React.MouseEvent<unknown>, id: number) => {
+  // //   const selectedIndex = selected.indexOf(id);
+  // //   let newSelected: readonly number[] = [];
 
-  const visibleRows = useMemo(
-    () =>
-      [...rows]
-        .sort(getComparator(order, orderBy))
-        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
-    [order, orderBy, page, rowsPerPage],
-  );
+  // //   if (selectedIndex === -1) {
+  // //     newSelected = newSelected.concat(selected, id);
+  // //   } else if (selectedIndex === 0) {
+  // //     newSelected = newSelected.concat(selected.slice(1));
+  // //   } else if (selectedIndex === selected.length - 1) {
+  // //     newSelected = newSelected.concat(selected.slice(0, -1));
+  // //   } else if (selectedIndex > 0) {
+  // //     newSelected = newSelected.concat(
+  // //       selected.slice(0, selectedIndex),
+  // //       selected.slice(selectedIndex + 1),
+  // //     );
+  // //   }
+  // //   setSelected(newSelected);
+  // // };
+
+  // const handleChangePage = (_: unknown, newPage: number) => {
+  //   setPage(newPage);
+  // };
+
+  // const handleChangeRowsPerPage = (
+  //   event: React.ChangeEvent<HTMLInputElement>,
+  // ) => {
+  //   setRowsPerPage(parseInt(event.target.value, 10));
+  //   setPage(0);
+  // };
+
+  // // const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
+  // //   setDense(event.target.checked);
+  // // };
+
+  // // Avoid a layout jump when reaching the last page with empty rows.
+  // const emptyRows =
+  //   page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  // const visibleRows = useMemo(
+  //   () =>
+  //     [...rows]
+  //       .sort(getComparator(order, orderBy))
+  //       .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+  //   [order, orderBy, page, rowsPerPage],
+  // );
 
   return (
     <>
